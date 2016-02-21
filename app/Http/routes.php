c321b6bae0f416275e9832ffb9a4d0874a6c4bd2
Aurélien Labate <aurelien-git@labate.me>
2016-02-21 01:58:49 +0100

    Force link to be generated in https

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,6 +5,18 @@
 | Application Routes
 |--------------------------------------------------------------------------
 |
+| The following line force each generated url to be in https.
+| If your have no https on your server, comment it.
+|
+*/
+
+URL::forceSchema("https");
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
